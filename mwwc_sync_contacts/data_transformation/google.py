85e2a2d428c2544a7ec80a9c3f44c6e55f25c142
Amo DelBello <amodelbello@protonmail.com>
2023-06-01 22:04:06 -0600

    Add broken google python sdk code

--- mwwc_sync_contacts/data_transformation/google.py
+++ mwwc_sync_contacts/data_transformation/google.py
@@ -1,18 +1,54 @@
 def sync_google_workspace(client):
-    # Call the Admin SDK Directory API
-    print("Getting the first 10 users in the domain")
-    results = (
-        client.users()
-        .list(customer="my_customer", maxResults=10, orderBy="email")
+    members = []
+    groups = []
+
+    # groups_result = (
+    #     client.groups()
+    #     .list(customer="my_customer", maxResults=10, orderBy="email")
+    #     .execute()
+    # )
+    # groups = groups_result.get("groups", [])
+
+    # list(groupKey, includeDerivedMembership=None, maxResults=None, pageToken=None, roles=None, x__xgafv=None)
+    # for i, group in enumerate(groups):
+    #     members_result = (
+    #         client.members()
+    #         .list(
+    #             groupKey=group["id"],
+    #             maxResults=10,
+    #             pageToken=str(i),
+    #             roles="MEMBER",
+    #         )
+    #         .execute()
+    #     )
+    #     members.append(members_result.get("members", []))
+    #     break
+    members_result = (
+        client.members()
+        .list(
+            groupKey="stewards@meowwolfworkers.org",
+            maxResults=10,
+        )
         .execute()
     )
-    users = results.get("users", [])
+    members = members_result.get("members", [])
 
-    if not users:
+    # privs_result = (
+    #     client.privileges()
+    #     .list(
+    #         customer="my_customer",
+    #     )
+    #     .execute()
+    # )
+    # privs = privs_result.get("items", [])
+
+    if not groups:
         print("No users in the domain.")
+        # return privs
     else:
-        response = []
-        for user in users:
-            full_name = user["name"]["fullName"]
-            response.append("{0} ({1})".format(user["primaryEmail"], full_name))
-        return response
+        return [groups, members]
+        # response = []
+        # for group in groups:
+        #     full_name = group["name"]["fullName"]
+        #     response.append("{0} ({1})".format(group["primaryEmail"], full_name))
+        # return response
