4d30c2b93d42ba2a1045990eb15623621ab80c12
Amo DelBello <amodelbello@protonmail.com>
2023-06-28 21:25:42 -0600

    Be more specific with airtable request

--- mwwc_sync_contacts/external_services/airtable.py
+++ mwwc_sync_contacts/external_services/airtable.py
@@ -2,9 +2,20 @@ from pyairtable import Table
 
 
 def get_banana_data(c):
+    fields = [
+        c["AIRTABLE_CHOSEN_FIRST_NAME"],
+        c["AIRTABLE_CHOSEN_LAST_NAME"],
+        c["AIRTABLE_BU_STATUS"],
+        c["AIRTABLE_AREA"],
+        c["AIRTABLE_MIGS"],
+        c["AIRTABLE_STEWARD"],
+        c["AIRTABLE_ELECTED_POSITION"],
+        c["AIRTABLE_PERSONAL_EMAIL"],
+    ]
+
     banana_table = Table(
         c["AIRTABLE_API_KEY"],
         c["AIRTABLE_BASE_ID"],
         c["AIRTABLE_TABLE_ID"],
     )
-    return banana_table.all()
+    return banana_table.all(view=c["AIRTABLE_VIEW_ID"], fields=fields)
