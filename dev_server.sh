611654d6aaf01c0961ee467c2c1b5e9057736530
Amo DelBello <amodelbello@protonmail.com>
2023-10-25 19:29:37 -0600

    Modify dev script to accommodate new structure

--- dev_server.sh
+++ dev_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" == "-d" ]; then
-    PYTHONBREAKPOINT=remote_pdb.set_trace REMOTE_PDB_HOST=127.0.0.1 REMOTE_PDB_PORT=4000 python -m flask --app mwwc_sync_contacts run --debug
+    PYTHONBREAKPOINT=remote_pdb.set_trace REMOTE_PDB_HOST=127.0.0.1 REMOTE_PDB_PORT=4000 python -m flask --app run --debug
 else
-    python -m flask --app mwwc_sync_contacts run --debug
+    python -m flask --app src run --debug
 fi
